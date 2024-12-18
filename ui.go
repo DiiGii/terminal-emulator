1535fb951910f8f718bdb80cabc1809037b363cc
Daniel Guo <dguo314other@gmail.com>
2024-12-17 20:18:42 -0800

    Implement pseudo-terminal
    * Connect with TTY driver in kernel
    * Using pty package, create a test terminal that runs the command "ls"

--- ui.go
+++ ui.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+	"time"
+	
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+
+	"github.com/creack/pty"
 )
 
 func main() {
@@ -14,6 +20,26 @@ func main() {
 	ui := widget.NewTextGrid()
 	ui.SetText("Terminal text!")
 	
+	c := exec.Command("/bin/bash")
+	p, err := pty.Start(c)
+
+	if err != nil {
+		fyne.LogError("Failed to open pty", err)
+		os.Exit(1)
+	}
+
+	defer c.Process.Kill()
+
+	p.Write([]byte("ls\r"))
+	time.Sleep(1 * time.Second)
+	b := make([]byte, 1024)
+	_, err = p.Read(b)
+	if err != nil {
+		fyne.LogError("Failed to read pty", err)
+	}
+
+	ui.SetText(string(b))
+
 	w.SetContent(
 		fyne.NewContainerWithLayout(
 			layout.NewGridWrapLayout(fyne.NewSize(420, 200)),
